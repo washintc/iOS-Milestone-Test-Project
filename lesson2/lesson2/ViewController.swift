7acc6aef8c5b6b02088af3805ea7770beb1a7534
epics-student <epics-student@pal-10-184-67-230.itap.purdue.edu>
2022-09-19 20:07:34 -0400

    created app

---
+++ lesson2/lesson2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lesson2
+//
+//  Created by epics-student on 9/19/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
